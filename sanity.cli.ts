5de3b88aa7ed632bbb793087408fe792c820da9e
Brandon Mitchell <brandonmitchell217@gmail.com>
2024-01-23 22:47:29 -0500

    [fix]: install dependencies

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,8 +1,8 @@
-// import { loadEnvConfig } from '@next/env'
+import { loadEnvConfig } from '@next/env'
 import { defineCliConfig } from 'sanity/cli'
 
 const dev = process.env.NODE_ENV !== 'production'
-// loadEnvConfig(__dirname, dev, { info: () => null, error: console.error })
+loadEnvConfig(__dirname, dev, { info: () => null, error: console.error })
 
 // @TODO report top-level await bug
 // Using a dynamic import here as `loadEnvConfig` needs to run before this file is loaded
